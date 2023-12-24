949608178ea6960a2f8b5e773f7a4df2c127b3a5
EltonArmelini <damotaelton@gmail.com>
2023-12-24 09:54:45 -0300

    Estructura Incial del Proyecto y configuracion de acceso a BD con PDO

---
+++ src/lib/Database.php
@@ -0,0 +1,40 @@
+<?php 
+
+
+namespace BasicApp\Noteapp\lib;
+
+use PDO;
+use PDOException;
+class Database {
+
+    private $host;
+    private $db;
+    private $user;
+    private $password;
+    private $charset;
+
+    public function __construct()
+    {
+        $this->host = 'localhost';
+        $this->db = 'noteapp';
+        $this->user = 'root';
+        $this->password = '';
+        $this->charset =  'utf8mb4';
+    }
+
+    public function connection(){
+        try {
+            $connection = "mysql:host={$this->host};dbname={$this->db};charset={$this->charset}"; 
+            $options = [
+                PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, 
+                PDO::ATTR_EMULATE_PREPARES=>false
+            ];
+            $pdo = new PDO($connection,$this->user,$this->password,$options);
+            return $pdo;
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+}
+
+ 
